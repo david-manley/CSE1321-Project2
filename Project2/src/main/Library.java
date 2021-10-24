ae581f0670f87187bc3002404e21278a1a302166
ncampb39 <90401547+ncampb39@users.noreply.github.com>
2021-10-24 18:31:12 -0400

    Update Library.java
    code for libraryFront

--- Project2/src/main/Library.java
+++ Project2/src/main/Library.java
@@ -12,6 +12,46 @@ public class Library {
 		System.out.println( "What will " + NAME + " do next? Right now it seems like they're just going to stand there like a statue, apparently");
 		String [] inputs = {"visit", "use", "look", "check inventory", "help"};
 		String input = input.nextLine();
-		
 	}
+	
+	public static String libraryFront
+	{
+		System.out.println(NAME + " walks into the library.");
+		System.out.println("So what are you doing while you're in here?: ");
+		command = checkInput(inputs);
+		
+		if(command=="visit"){
+			place=visit(place);
+		}
+		
+		else if(command=="help"){
+			help();	
+		}
+		
+		else if(command=="check inventory"){
+			checkInv();	
+		}
+		
+		else if(command=="look"){
+			System.out.println(NAME + " looks around the library and sees no comics in sight.");
+        		System.out.println("Well, of course there aren't comic books. This is a library not Barnes & Noble.");
+        		System.out.println(NAME +" does spot the section that has lots of old books when they finally stop moping about the comics.");
+        		System.out.println("You should be able to use the books through the books for information about the treasure.");
+		}
+		
+		else if(command=="use"){
+			if(!(hasItem("map"))){
+				System.out.println( NAME + " starts skimming through the books because they can't be bothered to actually read and a piece of paper falls out of the last book they were holding.");  
+        			System.out.println("That was convenient. The paper is a very faded map that’s hard to read.”);
+        			System.out.println("It looks like it might be a map of the cave, but I wouldn’t use that as my guide. You’d probably get lost or die or something.”);
+       				System.out.println("That being said you do whatever floats your boat, I'm just the unfortunate narrator that has to deal with you...");
+				addItemToInv("map");
+			}
+			else if(hasItem("map")){
+				PRINTLN (NAME + " starts skimming through the books yet again, because they can't be bothered to actually read.");
+			}
+				
+		}
+	}
+		
 }
