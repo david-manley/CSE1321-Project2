126c02d7e15e05e7c8d0aafc8ae5b53c06f61400
Rayan Shamsi <92473542+rshamsi05@users.noreply.github.com>
2021-10-24 00:17:48 -0400

    Update Library.java

--- Project2/src/main/Library.java
+++ Project2/src/main/Library.java
@@ -1,9 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 public class Library {
 
-	public Library()
+	public static String libraryEntrance
 	{
+		System.out.println(NAME + "is at the library entrance right now");
+		System.out.println(NAME + " was thinking of checking out the Library to see if any useful information will be there and if the library has any new comics!");
+		System.out.println("Narrator: " + NAME + " needs a Library Pass to enter the back section." + NAME +" remembers what happened last time someone forgot it and they lost their pants!!");
+		System.out.println( "What will " + NAME + " do next? Right now it seems like they're just going to stand there like a statue, apparently");
+		String [] inputs = {"visit", "use", "look", "check inventory", "help"};
+		String input = input.nextLine();
 		
 	}
 }
